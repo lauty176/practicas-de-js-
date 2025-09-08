597a55387788177493d4dfcfa56434781f5cb68c
lautaro perez <lautaropd019@gmail.com>
2025-09-08 19:54:17 -0300

    practica 1

---
+++ introduccion.js/introduccion.js
@@ -0,0 +1,8 @@
+console.log("hola mundo");
+let edad = 18 ;
+let nombre = " lautaro";
+let apellido = " perez diaz ";
+let esmayordeedad = true ; 
+console.log(nombre)
+console.log ( apellido)
+console.log ( esmayordeedad)
